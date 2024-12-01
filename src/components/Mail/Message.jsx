74ad0ca70c26b951741240d19a02a7a75ae4c9e9
gautamranjan09 <gautamranjan96@gmail.com>
2024-12-01 23:29:26 +0530

    feat: added states for selecting and starring mails to improve email management functionality

--- src/components/Mail/Message.jsx
+++ src/components/Mail/Message.jsx
@@ -1,36 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdCropSquare } from "react-icons/md";
-import { RiStarLine } from "react-icons/ri";
+import { RiStarLine, RiStarSFill } from "react-icons/ri";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useStripHTML } from "../hooks/useStripHTML";
+import { IoMdCheckboxOutline } from "react-icons/io";
 
 const Message = ({ email, index }) => {
   const selectedMailPath = useSelector((state) => state.navSlice.selectedMailPath);
   const navigate = useNavigate();
+
+  const [selectSquare, setSelectSquare] = useState(false); // for selecting a particular box
+  const [selectStarred, setSelectStarred] = useState(false); // for selecting starred box
+
+  const handleSelectedEmail = (event) => {
+    event.stopPropagation();
+    setSelectSquare(!selectSquare);
+  };
+
+  const handleSelectedStarredEmail = (event) => {
+    event.stopPropagation();
+    setSelectStarred(!selectStarred);
+  };
+
   const openMail = () => {
     navigate(`/${selectedMailPath}/${email.id}`);
   };
-  
 
   return (
-    <div onClick={openMail} style={{ '--delay': `${index * 100}ms` }} className="flex items-start justify-between border-b border-gray-200 px-4 py-2 text-sm cursor-pointer  hover:shadow-md hover:translate-y-1 hover:bg-rose-300/40 transition-all duration-1000 ease-in-out animate-slideUp opacity-0 [animation-delay:var(--delay)]">
-        <div className="flex items-center gap-3">
-            <div className="flex-none text-gray-300">
-                <MdCropSquare className="w-5 h-5" /> 
-                {/* {improve} */}
-            </div>
-            <div className="flex-none text-gray-300">
-                <RiStarLine className="w-5 h-5" />
-            </div>
+    <div
+      onClick={openMail}
+      style={{ "--delay": `${index * 100}ms` }}
+      className="flex items-center justify-between border-b border-gray-200 px-4 text-sm cursor-pointer  hover:shadow-md hover:translate-y-1 hover:bg-rose-300/40 transition-all duration-1000 ease-in-out animate-slideUp opacity-0 [animation-delay:var(--delay)]"
+    >
+      <div className="flex items-center gap-3">
+        <div className="flex-none text-gray-300 px-1 py-2" onClick={handleSelectedEmail}>
+          {selectSquare ? <IoMdCheckboxOutline className="w-5 h-5 text-teal-400" /> : <MdCropSquare className="w-5 h-5" />}
+          {/* {improve} */}
         </div>
-        <div className="flex-1 ml-4 flex items-center">
-            <p className="text-black font-bold  w-72">{(selectedMailPath !== "sent" && email?.from) || (selectedMailPath === "sent" && email?.to)}</p>
-            <p className="text-gray-600 truncate inline-block w-[35rem]"><strong>{email?.subject}</strong>-{useStripHTML(email?.message)}</p>
+        <div className="flex-none text-gray-300 px-1 py-2" onClick={handleSelectedStarredEmail}>
+          {selectStarred ? <RiStarSFill className="w-5 h-5 text-teal-400" /> : <RiStarLine className="w-5 h-5" />}
         </div>
-        <div className="flex-none text-gray-400 text-sm">
-            <p>{email?.createdAt}</p>
-        </div>
+      </div>
+      <div className="flex-1 ml-4 flex items-center">
+        <p className="text-black font-bold  w-72">{(selectedMailPath !== "sent" && email?.from) || (selectedMailPath === "sent" && email?.to)}</p>
+        <p className="text-gray-600 truncate inline-block w-[35rem]">
+          <strong>{email?.subject}</strong>-{useStripHTML(email?.message)}
+        </p>
+      </div>
+      <div className="flex-none text-gray-400 text-sm">
+        <p>{email?.createdAt}</p>
+      </div>
     </div>
   );
 };
