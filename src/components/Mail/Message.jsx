ab3f01178a80c9bdf5ed612ca9daab4c88e9e3fe
gautamranjan09 <gautamranjan96@gmail.com>
2024-12-03 22:03:46 +0530

    feat: implemented email restore from trash and permanent delete functionality
    - Added functionality to restore emails from the trash back to their original folder.
    - Implemented permanent email deletion from the trash for better email management.
    - Enhanced Trash component to support restore and delete actions with improved UI feedback.

--- src/components/Mail/Message.jsx
+++ src/components/Mail/Message.jsx
@@ -9,6 +9,7 @@ import { doc, setDoc, updateDoc } from "firebase/firestore";
 import { toast } from "react-toastify";
 import { db } from "../../firebase";
 import { setSelectedEmailsArray } from "../../redux/appSlice";
+import { TbTrashX } from "react-icons/tb";
 
 const Message = ({ email, index }) => {
   const selectedMailPath = useSelector((state) => state.navSlice.selectedMailPath);
@@ -18,22 +19,22 @@ const Message = ({ email, index }) => {
 
   const [selectSquare, setSelectSquare] = useState(false); // for selecting a particular box
 
- useEffect(()=>{
-  if(selectedEmailsArray.includes(email)){
-    setSelectSquare(true);
-  } else{
-    setSelectSquare(false);
-  }
- },[selectedEmailsArray])
+  useEffect(() => {
+    if (selectedEmailsArray.includes(email)) {
+      setSelectSquare(true);
+    } else {
+      setSelectSquare(false);
+    }
+  }, [selectedEmailsArray]);
 
   const handleSelectedEmail = (event) => {
     event.stopPropagation();
     //setSelectSquare(!selectSquare);
-    if(selectSquare === true){
+    if (selectSquare === true) {
       setSelectSquare(false);
       const updatedArray = selectedEmailsArray.filter((currentEmail) => currentEmail.id !== email.id);
       dispatch(setSelectedEmailsArray(updatedArray));
-    }else{
+    } else {
       setSelectSquare(true);
       dispatch(setSelectedEmailsArray([...selectedEmailsArray, email]));
     }
@@ -42,14 +43,25 @@ const Message = ({ email, index }) => {
   const handleSelectedStarredEmail = async (event) => {
     event.stopPropagation();
     const starredStatus = email?.starred ? false : true;
-    try{
-     const updatedEmail= await updateDoc(doc(db, "emails", email.id),{
-        starred: starredStatus
-      })
-
-    }catch(error){
+    try {
+      await updateDoc(doc(db, "emails", email.id), {
+        starred: starredStatus,
+      });
+    } catch (error) {
       toast.error(error.message);
-    } 
+    }
+  };
+
+  const handleSelectedTrashedEmail = async (event) => {
+    event.stopPropagation();
+    const trashedStatus = email?.trashed ? false : true;
+    try {
+      await updateDoc(doc(db, "emails", email.id), {
+        trashed: trashedStatus,
+      });
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   const openMail = () => {
@@ -62,7 +74,7 @@ const Message = ({ email, index }) => {
       style={{ "--delay": `${index * 100}ms` }}
       className="flex items-center justify-between border-b border-gray-200 px-4 text-sm cursor-pointer  hover:shadow-md hover:translate-y-1 hover:bg-rose-300/40 transition-all duration-1000 ease-in-out animate-slideUp opacity-0 [animation-delay:var(--delay)]"
     >
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-1">
         <div className="flex-none text-gray-300 px-1 py-2" onClick={handleSelectedEmail}>
           {selectSquare ? <IoMdCheckboxOutline className="w-5 h-5 text-teal-400" /> : <MdCropSquare className="w-5 h-5" />}
           {/* {improve} */}
@@ -70,10 +82,13 @@ const Message = ({ email, index }) => {
         <div className="flex-none text-gray-300 px-1 py-2" onClick={handleSelectedStarredEmail}>
           {email?.starred ? <RiStarSFill className="w-5 h-5 text-teal-400" /> : <RiStarLine className="w-5 h-5" />}
         </div>
+        <div className="flex-none text-gray-300 px-1 py-2" onClick={handleSelectedTrashedEmail}>
+          {email?.trashed ? <TbTrashX className="w-5 h-5 text-rose-600 hover:text-teal-400" /> : <TbTrashX className="w-5 h-5 hover:text-rose-600" />}
+        </div>
       </div>
       <div className="flex-1 ml-4 flex items-center">
         <p className="text-black font-bold  w-72">{(selectedMailPath !== "sent" && email?.from) || (selectedMailPath === "sent" && email?.to)}</p>
-        <p className="text-gray-600 truncate inline-block w-[35rem]">
+        <p className="text-gray-600 truncate inline-block w-[32rem]">
           <strong>{email?.subject}</strong>-{useStripHTML(email?.message)}
         </p>
       </div>
--- src/components/UI/UiEmailTypeBody.jsx
+++ src/components/UI/UiEmailTypeBody.jsx
@@ -6,7 +6,7 @@ import { GoTag } from "react-icons/go";
 import { FaTrash, FaUserFriends } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
-import { doc, updateDoc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase";
 import { setSelectedEmailsArray } from "../../redux/appSlice";
 
@@ -27,6 +27,7 @@ const mailType = [
 const UiEmailTypeBody = ({ setNoOfMailOnCurrPage, noOfMailOnCurrPage }) => {
   const [mailTypeSelected, setMailTypeSelected] = useState("Primary");
   const selectedEmailsArray = useSelector((state) => state.appSlice.selectedEmailsArray);
+  const selectedMailPath = useSelector((state) => state.navSlice.selectedMailPath);
   const totalNumOfMails = useSelector((state) => state.navSlice.totalNumOfMails);
   const dispatch = useDispatch();
 
@@ -38,26 +39,93 @@ const UiEmailTypeBody = ({ setNoOfMailOnCurrPage, noOfMailOnCurrPage }) => {
     if (noOfMailOnCurrPage > 0) setNoOfMailOnCurrPage((prevNoOfMailOnCurrPage) => prevNoOfMailOnCurrPage - 20);
   };
 
-  const handleTrashEmail = async() => {
+  const handleTrashEmail = async () => {
     // handle trash email logic here
-    
-    const updatedEmailPromise=  selectedEmailsArray.map((email)=>{
-      const trashStatus = email?.trashed ? false : true;
-      try{
-        const deletePromise = updateDoc(doc(db, "emails", email.id),{
-           trashed: trashStatus
-         })
-         return deletePromise;
-       }catch(error){
-         toast.error(error.message);
-       } 
+
+    // Show initial loading toast
+    const loadingToast = toast.loading("Moving to trash...");
+
+    const updatedEmailPromise = selectedEmailsArray.map((email) => {
+      try {
+        const deletePromise = updateDoc(doc(db, "emails", email.id), {
+          trashed: true,
+        });
+        return deletePromise;
+      } catch (error) {
+        toast.error(error.message);
+      }
     });
 
     // Wait for all actions to complete
     await Promise.all(updatedEmailPromise);
     dispatch(setSelectedEmailsArray([]));
-    toast.success( "Email successfully moved to trash!");
-  }
+
+    // Update the loading toast with success message
+    toast.update(loadingToast, {
+      render: `Email successfully moved to trash!`,
+      type: "success",
+      isLoading: false,
+      autoClose: 3000,
+    });
+  };
+
+  const handleRestoreEmail = async () => {
+    // handle restore email logic here
+
+    // Show initial loading toast
+    const loadingToast = toast.loading("Restoring emails...");
+
+    const updatedEmailPromise = selectedEmailsArray.map((email) => {
+      try {
+        const deletePromise = updateDoc(doc(db, "emails", email.id), {
+          trashed: false,
+        });
+        return deletePromise;
+      } catch (error) {
+        toast.error(error.message);
+      }
+    });
+
+    // Wait for all actions to complete
+    await Promise.all(updatedEmailPromise);
+    dispatch(setSelectedEmailsArray([]));
+
+    // Update the loading toast with success message
+    toast.update(loadingToast, {
+      render: `Emails restored successfully!`,
+      type: "success",
+      isLoading: false,
+      autoClose: 3000,
+    });
+  };
+
+  const handleDeleteEmail = async () => {
+    // handle restore email logic here
+
+    // Show initial loading toast
+    const loadingToast = toast.loading("Deleting emails...");
+
+    const updatedEmailPromise = selectedEmailsArray.map((email) => {
+      try {
+        const deletePromise = deleteDoc(doc(db, "emails", email.id));
+        return deletePromise;
+      } catch (error) {
+        toast.error(error.message);
+      }
+    });
+
+    // Wait for all actions to complete
+    await Promise.all(updatedEmailPromise);
+    dispatch(setSelectedEmailsArray([]));
+
+    // Update the loading toast with success message
+    toast.update(loadingToast, {
+      render: `Emails deleted successfully!`,
+      type: "success",
+      isLoading: false,
+      autoClose: 3000,
+    });
+  };
 
   return (
     <>
@@ -70,9 +138,21 @@ const UiEmailTypeBody = ({ setNoOfMailOnCurrPage, noOfMailOnCurrPage }) => {
           <div className="p-2 rounded-full hover:bg-teal-200/30 transition-all duration-500 ease-in-out hover:rotate-12">
             <IoMdRefresh size={"20px"} />
           </div>
-          {selectedEmailsArray.length > 0 && <div onClick={handleTrashEmail} className="p-2 rounded-full hover:bg-teal-200/30 transition-all duration-500 ease-in-out">
-            <FaTrash size={"16px"} />
-          </div>}
+          {selectedEmailsArray.length > 0 && selectedMailPath !== "trash" && (
+            <div onClick={handleTrashEmail} className="p-2 rounded-full hover:bg-teal-200/30 transition-all duration-500 ease-in-out">
+              <FaTrash size={"16px"} />
+            </div>
+          )}
+          {selectedEmailsArray.length > 0 && selectedMailPath === "trash" && (
+            <div onClick={handleRestoreEmail} className="p-1 rounded-md hover:text-teal-700 hover:font-semibold hover:bg-teal-200/30 transition-all duration-500 ease-in-out text-black">
+              <div>Restore</div>
+            </div>
+          )}
+          {selectedEmailsArray.length > 0 && selectedMailPath === "trash" && (
+            <div onClick={handleDeleteEmail} className="p-1 rounded-md hover:text-rose-700 hover:font-semibold hover:bg-rose-200/30 transition-all duration-500 ease-in-out text-black">
+              <div>Delete forever</div>
+            </div>
+          )}
           <div className="p-2 rounded-full hover:bg-teal-200/30 transition-all duration-500 ease-in-out">
             <IoMdMore size={"20px"} />
           </div>
